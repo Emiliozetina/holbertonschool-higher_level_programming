9987d913cfc25b77e430557523e18051f9959187
Emilio Zetina <emilio.zetina@anahuacmayab.edu.mx>
2026-01-28 22:31:40 +0000

    update excercise 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Create database hbtn_0d_usa and table states
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
        id INT NOT NULL AUTO_INCREMENT,
