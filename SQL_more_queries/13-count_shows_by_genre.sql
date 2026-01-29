777850187c4bd029efaad4dbbea6097212becd85
Emilio Zetina <emilio.zetina@anahuacmayab.edu.mx>
2026-01-29 00:48:04 +0000

    FUCKING FIXED

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,6 @@
 -- 13-count_shows_by_genre.sql
--- List all genres with the number of linked TV shows, sorted descending
-
-SELECT g.name AS genre, COUNT(tg.tv_show_id) AS number_of_shows
-FROM genres g
+SELECT g.name AS genre, COUNT(tg.show_id) AS number_of_shows
+FROM tv_genres g
 JOIN tv_show_genres tg ON g.id = tg.genre_id
 GROUP BY g.name
 ORDER BY number_of_shows DESC;
