2f306d25d46d5a85988a14ee974a0c5bac1b44ec
Emilio Zetina <emilio.zetina@anahuacmayab.edu.mx>
2026-01-29 00:40:50 +0000

    final I think

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,8 @@
 -- Show the number of shows per genre
-SELECT g.name AS genre, COUNT(tg.show_id) AS number_of_shows
+SELECT g.name AS genre, COUNT(tg.tv_show_id) AS number_of_shows
 FROM genres g
-JOIN tv_show_genres tg
+JOIN tv_show_genres tg 
 ON g.id = tg.genre_id
 GROUP BY g.name
-ORDER BY number_of_shows DESC, genre ASC
+HAVING COUNT(tg.tv_show_id) > 0
+ORDER BY number_of_shows DESC;
